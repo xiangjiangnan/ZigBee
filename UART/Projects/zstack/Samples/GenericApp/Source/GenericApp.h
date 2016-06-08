b37a41e599d6772d2a3e61b0a85a50847c23f24d
xiangjiangnan <xjn19920918@126.com>
2016-06-08 14:09:53 +0800

    uart communication

---
+++ UART/Projects/zstack/Samples/GenericApp/Source/GenericApp.h
@@ -0,0 +1,36 @@
+#ifndef GENERICAPP_H
+#define GENERICAPP_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "ZComDef.h"
+extern byte GenericApp_TaskID;
+
+/*********************************************************************
+ * FUNCTIONS
+ */
+
+/*
+ * Task Initialization for the Generic Application
+ */
+extern void GenericApp_Init( byte task_id );
+
+/*
+ * Task Event Processor for the Generic Application
+ */
+extern UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events );
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GENERICAPP_H */
---
+++ UART/UART/Projects/zstack/Samples/GenericApp/CC2530DB/AT_UART.h
@@ -0,0 +1,4 @@
+#define AT_UART_RX_BUFF_MAX     128
+#define AT_UART_TX_BUFF_MAX     128
+
+extern void AT_UART_init(void);
